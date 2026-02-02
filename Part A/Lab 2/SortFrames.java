32a203b613b5abd147c9c16c1fd3c27138f92fb5
aneeshs05 <aneeshappid@gmail.com>
2026-02-02 23:43:46 +0530

    Sort Frames

--- Part A/Lab 2/SortFrames.java
+++ Part A/Lab 2/SortFrames.java
@@ -1,58 +1,64 @@
-import java.util.*;
+/*Write a program to sort frames using appropriate sorting techniques*/
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class SortFrames {
     public static void main(String[] args) {
-        ArrayList<int[]> frames= new ArrayList<>();
-        Random random=new Random();
-        Scanner sc=new Scanner(System.in);
+        List<int[]> frame = new ArrayList<>();
+        System.out.println("Enter no. of frames:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        System.out.println("Enter the number of frames");
-        int num=sc.nextInt();
-
-        System.out.print("\n--------\n");
-
-        for(int i=0; i<num; i++){
-            System.out.printf("Enter data for frame %d: ",i+1);
-            int data=sc.nextInt();
-            int seqNum=random.nextInt(500);
-            int[] frame={seqNum,data};
-            frames.add(frame);
+        for (int i = 0; i < n; i++) {
+            Random random = new Random();
+            int seqNum = random.nextInt(1000) + 1;
+            System.out.printf("Enter data for %dth frame>>", i + 1);
+            int data = sc.nextInt();
+            frame.add(new int[]{seqNum, data});
         }
-        System.out.print("\n--------\n");
 
-        System.out.println("The frames before sorting are:");
-        for(int[] frame:frames){
-            System.out.printf("%d : %d\n",frame[0], frame[1]);
+        System.out.println("\n\nBefore Sorting>>");
+        for (int[] i : frame) {
+            System.out.printf("seqNum->%d, Data->%d\n", i[0], i[1]);
         }
-        System.out.print("\n--------\n");
 
-        Collections.sort(frames, (a,b)-> Integer.compare(a[0],b[0]));
+        frame = sortFrame(frame);
 
-        System.out.println("The frames after sorting are:");
-        for(int[] frame:frames){
-            System.out.printf("%d : %d\n",frame[0], frame[1]);
+        System.out.println("\n\n After sorting>>");
+        for (int[] i : frame) {
+            System.out.printf("seqNum->%d, Data->%d\n", i[0], i[1]);
         }
         sc.close();
-    }    
+    }
+
+    public static List<int[]> sortFrame(List<int[]> frame) {
+        Collections.sort(frame, (a, b) -> Integer.compare(a[0], b[0]));
+        return frame;
+    }
 }
 
-// // Output
-// Enter the number of frames
-// 3
+/*output
+Enter no. of frames:
+4
+Enter data for 1th frame>>342
+Enter data for 2th frame>>256
+Enter data for 3th frame>>890
+Enter data for 4th frame>>356
 
-// --------
-// Enter data for frame 1: 2 
-// Enter data for frame 2: 232
-// Enter data for frame 3: 433
 
-// --------
-// The frames before sorting are:
-// 235 : 2
-// 141 : 232
-// 375 : 433
+Before Sorting>>
+seqNum->703, Data->342
+seqNum->654, Data->256
+seqNum->704, Data->890
+seqNum->876, Data->356
 
-// --------
-// The frames after sorting are:
-// 141 : 232
-// 235 : 2
-// 375 : 433
+
+ After sorting>>
+seqNum->654, Data->256
+seqNum->703, Data->342
+seqNum->704, Data->890
+seqNum->876, Data->356
+*/
